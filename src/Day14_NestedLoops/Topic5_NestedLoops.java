51a602c7a4b43ce1b3d319bca620c98f789f74c7
Eloise Elemen <eloise.elemen@gmail.com>
2022-11-16 13:35:34 -0500

    Day24 and StudySession Week9

--- src/Day14_NestedLoops/Topic5_NestedLoops.java
+++ src/Day14_NestedLoops/Topic5_NestedLoops.java
@@ -5,12 +5,12 @@ public class Topic5_NestedLoops {
 
         for (int i = 0; i < 2; i++) {
 
-            System.out.println("this is the first Outer Loop value of i: " + i); // this code will get executed 3
+            System.out.println("this is the first Outer Loop value of i: " + i); // this code will get executed 2
             for (int j = 0; j < 3; j++) {
-                System.out.println("\t this is second Outer/ first Inner Loop i: " + i + " j: " + j); // this code will get executed 9
+                System.out.println("\t this is second Outer/ first Inner Loop i: " + i + " j: " + j); // this code will get executed 6
 
                 for (int k = 0; k < 4; k++) {
-                    System.out.println("\t \tthis second Inner Loop i:" + i + " j: " + j + " k:" + k); // this code will get executed 27
+                    System.out.println("\t \tthis second Inner Loop i:" + i + " j: " + j + " k:" + k); // this code will get executed 24
 
                 }
 
