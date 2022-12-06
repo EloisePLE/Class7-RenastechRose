b27472aba143f22641afd0b3617e65541690afa9
Eloise Elemen <eloise.elemen@gmail.com>
2022-12-06 08:37:56 -0500

    Day34 cont

---
+++ src/Day34_InterviewPractiseString/Topic1_ReverseString.java
@@ -0,0 +1,39 @@
+package Day34_InterviewPractiseString;
+
+public class Topic1_ReverseString {
+    public static void main(String[] args) {
+        System.out.println(USingStringBuilder("This is an example"));
+        System.out.println("*********");
+        System.out.println(RegularWay("This is an example"));
+        System.out.println("*********");
+        System.out.println(RegularWayWithForEach("This is an example"));
+    }
+
+    public static String USingStringBuilder(String param1) {
+        StringBuilder SB = new StringBuilder(param1);
+        SB.reverse();
+        return SB.toString();
+    }
+
+    public static String RegularWay(String param1) {
+        char[] MyCharArray = param1.toCharArray();
+        String ReversedString = "";
+        for (int i = 0; i < MyCharArray.length; i++) {
+            ReversedString = MyCharArray[i] + ReversedString;
+        }
+
+        return ReversedString;
+    }
+
+    public static String RegularWayWithForEach(String param1) {
+        char[] MyCharArray = param1.toCharArray();
+
+        String ReversedString = "";
+
+        for (char MyChar : MyCharArray) {
+//            MyChar = MyCharArray[i]
+            ReversedString = MyChar + ReversedString;
+        }
+        return ReversedString;
+    }
+}
